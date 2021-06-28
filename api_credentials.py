796f0fd35a8017ae7d7e8f5a62cd29f4304dc2bc
Fernando Pérez <34141678+Fernandop26@users.noreply.github.com>
2021-06-28 17:21:07 +0200

    Add files via upload

---
+++ api_credentials.py
@@ -0,0 +1,4 @@
+consumer_key = "eUku1HIAjCeGbHJQJOhSKReRI"
+consumer_secret = "84woqMNHuiN266vXZ3MHlWOPA1U8yJx6rqyk0KqNNCTOwMggoX"
+token_key = "1357347298846515202-GxXvjPe37xaN0PVx79AP63tpxnuB3T"
+token_secret = "uVqMfvvWEIIGZBh4sXsxvOozbT8xyZzzaclISv998gV4x"
